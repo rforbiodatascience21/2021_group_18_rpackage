54cf4574900b490097f65c76bbf4ba033f0ae023
SofiaOtero <s164527@student.dtu.dk>
2021-03-22 09:14:01 +0000

    help to dna_codons

--- R/dna_codons_to_aa.R
+++ R/dna_codons_to_aa.R
@@ -1,7 +1,26 @@
+# R/dna_codons_to_aa.R
+
+#' translates dna codes to amino acids
+#' 
+#' @param codons a vector with strings containing dna codes e.g. "TTT"
+#' @return a sequence of amino acids
+
 dna_codons_to_aa <- function(codons){
   std_code_table <- c("TTT" = "F", "TCT" = "S", "TAT" = "Y", "TGT" = "C",
                       "TTC" = "F", "TCC" = "S", "TAC" = "Y", "TGC" = "C",
                       "TTA" = "L", "TCA" = "S", "TAA" = "*", "TGA" = "*",
+                      "TTG" = "L", "TCG" = "S", "TAG" = "*", "TGG" = "W",
+                      "CTT" = "L", "CCT" = "P", "CAT" = "H", "CGT" = "R",
+                      "CTT" = "L", "CCT" = "P", "CAT" = "H", "CGT" = "R",
+                      "CTT" = "L", "CCT" = "P", "CAT" = "H", "CGT" = "R",
+                      "CTT" = "L", "CCT" = "P", "CAT" = "H", "CGT" = "R",
+                      "CTT" = "L", "CCT" = "P", "CAT" = "H", "CGT" = "R",
+                      "CTT" = "L", "CCT" = "P", "CAT" = "H", "CGT" = "R",
+                      "CTT" = "L", "CCT" = "P", "CAT" = "H", "CGT" = "R",
+                      "CTT" = "L", "CCT" = "P", "CAT" = "H", "CGT" = "R",
+                      "CTT" = "L", "CCT" = "P", "CAT" = "H", "CGT" = "R",
+                      "CTC" = "L", "CCC" = "P", "CAC" = "H", "CGC" = "R",
+                      "CTA" = "L", "TCA" = "S", "TAA" = "*", "TGA" = "*",
                       "TTG" = "L", "TCG" = "S", "TAG" = "*", "TGG" = "W",
                       "CTT" = "L", "CCT" = "P", "CAT" = "H", "CGT" = "R",
                       "CTC" = "L", "CCC" = "P", "CAC" = "H", "CGC" = "R",
